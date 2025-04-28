32c33ddecd93f1459ba99f1e6b2b0004c32aad70
望 <1501034130@qq.com>
2025-04-28 14:29:31 +0800

    feat: 消息输入框新增发送按钮

--- packages/app/src/components/chat/ChatInput.vue
+++ packages/app/src/components/chat/ChatInput.vue
@@ -1,11 +1,21 @@
 <template>
   <form @submit.prevent.stop="emits('submit')" class="chat-Input">
     <input v-model="content" ref="input" type="text" />
+    <Button
+      label="Send"
+      type="submit"
+      icon="ti ti-send"
+      severity="primary"
+      size="small"
+      variant="text"
+      :disabled="content.length === 0"
+    />
   </form>
 </template>
 
 <script setup lang="ts">
 import { useTemplateRef } from 'vue'
+import { Button } from 'primevue'
 
 defineOptions({
   name: 'ChatInput',
